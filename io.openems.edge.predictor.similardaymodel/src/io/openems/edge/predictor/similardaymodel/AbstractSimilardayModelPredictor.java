a589d927039e6ffc19dcd3e4ed35af1242b442c6
venu-sagar <sagar.venu@fenecon.de>
2020-02-26 14:56:29 +0100

    created a new projevct for similar day model

---
+++ io.openems.edge.predictor.similardaymodel/src/io/openems/edge/predictor/similardaymodel/AbstractSimilardayModelPredictor.java
@@ -0,0 +1,213 @@
+package io.openems.edge.predictor.similardaymodel;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
+import io.openems.common.types.ChannelAddress;
+import io.openems.common.utils.JsonUtils;
+import io.openems.edge.common.component.AbstractOpenemsComponent;
+import io.openems.edge.common.component.ComponentManager;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.event.EdgeEventConstants;
+import io.openems.edge.predictor.api.HourlyPrediction;
+import io.openems.edge.predictor.api.HourlyPredictor;
+import io.openems.edge.timedata.api.Timedata;
+
+public abstract class AbstractSimilardayModelPredictor extends AbstractOpenemsComponent implements HourlyPredictor {
+
+	private final Logger log = LoggerFactory.getLogger(AbstractSimilardayModelPredictor.class);
+
+	private final ChannelAddress channelAddress;
+	private final Clock clock;
+	private boolean executed;
+	private long currentEnergy;
+	LocalDateTime prevHour = LocalDateTime.now();
+
+	private final TreeMap<LocalDateTime, Integer> hourlyEnergyData = new TreeMap<LocalDateTime, Integer>();
+
+	protected AbstractSimilardayModelPredictor(Clock clock, String componentId,
+			io.openems.edge.common.channel.ChannelId channelId) {
+		super(//
+				OpenemsComponent.ChannelId.values(), //
+				PredictorChannelId.values() //
+		);
+		this.channelAddress = new ChannelAddress(componentId, channelId.id());
+		this.clock = clock;
+	}
+
+	protected AbstractSimilardayModelPredictor(String componentId,
+			io.openems.edge.common.channel.ChannelId channelId) {
+		this(Clock.systemDefaultZone(), componentId, channelId);
+	}
+
+	protected abstract ComponentManager getComponentManager();
+	
+	protected abstract Timedata getTimedata();
+
+	/**
+	 * Collects the persistence model data on every cycle.
+	 * 
+	 * @param event the Event provided by {@link EventHandler}.
+	 */
+	public void handleEvent(Event event) {
+		if (!this.isEnabled()) {
+			return;
+		}
+		switch (event.getTopic()) {
+		case EdgeEventConstants.TOPIC_CYCLE_AFTER_PROCESS_IMAGE:
+			try {
+				//this.calculateEnergyValue();
+				this.getData();
+				this.channel(PredictorChannelId.UNABLE_TO_PREDICT).setNextValue(false);
+			} catch (OpenemsNamedException e) {
+				this.logError(this.log, e.getMessage());
+				this.channel(PredictorChannelId.UNABLE_TO_PREDICT).setNextValue(true);
+			}
+
+		}
+	}
+	
+	@SuppressWarnings("null")
+	private void getData() throws OpenemsNamedException {
+		
+		ZonedDateTime fromDate = ZonedDateTime.of(2020, 02, 10, 0, 0, 0, 0, ZoneId.of("UTC"));
+		ZonedDateTime toDate = ZonedDateTime.of(2020, 02, 12, 0, 0, 0, 0, ZoneId.of("UTC"));
+		TreeSet<ChannelAddress> channels = new TreeSet<>();
+		channels.add(this.channelAddress);
+		
+		SortedMap<ZonedDateTime, SortedMap<ChannelAddress, JsonElement>> Data = this.getTimedata()
+				.queryHistoricData(null, fromDate, toDate, channels,
+						60 * 60 /* 15 Minutes */);
+		JsonObject result = getResult(Data);
+//		JsonArray jArray = (JsonArray) result.get("timestamps");
+//		rework(jArray);
+	}
+
+//	public void rework(JsonArray str) {
+//		int len = str.size();
+//		for (int i = 0; i <= len ; i++) {
+//			String[] newArr1  = Arrays.copyOfRange(str, 0, len/2);
+//			String[] newArr2  = Arrays.copyOfRange(str, str.length/2, str.length);
+//			
+//			System.out.println(Arrays.toString(newArr1));
+//			System.out.println(Arrays.toString(newArr2));
+//		}
+//	}
+
+			
+			
+			
+			
+			
+
+	
+	public JsonObject getResult(SortedMap<ZonedDateTime, SortedMap<ChannelAddress, JsonElement>> table) {
+		JsonObject result = new JsonObject();
+
+		JsonArray timestamps = new JsonArray();
+		for (ZonedDateTime timestamp : table.keySet()) {
+			timestamps.add(timestamp.format(DateTimeFormatter.ISO_INSTANT));
+		}
+		result.add("timestamps", timestamps);
+
+		JsonObject data = new JsonObject();
+		for (Entry<ZonedDateTime, SortedMap<ChannelAddress, JsonElement>> rowEntry : table.entrySet()) {
+			for (Entry<ChannelAddress, JsonElement> colEntry : rowEntry.getValue().entrySet()) {
+				String channelAddress = colEntry.getKey().toString();
+				JsonElement value = colEntry.getValue();
+				JsonElement channelValuesElement = data.get(channelAddress);
+				JsonArray channelValues;
+				if (channelValuesElement != null) {
+					channelValues = channelValuesElement.getAsJsonArray();
+				} else {
+					channelValues = new JsonArray();
+				}
+				channelValues.add(value);
+				data.add(channelAddress, channelValues);
+			}
+		}
+		result.add("data", data);
+
+		return result;
+	}
+	
+
+
+	
+
+	
+	
+//	/*
+//	 * This method gets the value from the Channel every one hour and updates the
+//	 * TreeMap.
+//	 */
+//	private void calculateEnergyValue() throws OpenemsNamedException {
+//		LongReadChannel channel = this.getComponentManager().getChannel(this.channelAddress);
+//		Optional<Long> energyOpt = channel.value().asOptional();
+//
+//		// Stop early if there is no energy available (yet)
+//		if (!energyOpt.isPresent()) {
+//			return;
+//		}
+//		long energy = energyOpt.get();
+//
+//		LocalDateTime currentHour = LocalDateTime.now(this.clock).withNano(0).withMinute(0).withSecond(0);
+//
+//		if (!executed) {
+//			// First time execution - Map is still empty
+//			this.currentEnergy = energy;
+//			this.prevHour = currentHour;
+//			this.executed = true;
+//		} else if (currentHour.isAfter(this.prevHour)) {
+//			// hour changed -> calculate delta and record value
+//			int delta = (int) (energy - this.currentEnergy);
+//			this.hourlyEnergyData.put(this.prevHour, delta);
+//			this.prevHour = currentHour;
+//			this.currentEnergy = energy;
+//		} else {
+//			// hour did not change -> return
+//			return;
+//		}
+//
+//		// We added an entry to the map. Implement circular buffer.
+//		if (this.hourlyEnergyData.size() > 24) {
+//			this.hourlyEnergyData.remove(this.hourlyEnergyData.firstKey());
+//		}
+//	}
+
+	@Override
+	public HourlyPrediction get24hPrediction() {
+		Integer[] values = new Integer[24];
+		int i = Math.max(0, 24 - this.hourlyEnergyData.size());
+
+		for (Entry<LocalDateTime, Integer> entry : this.hourlyEnergyData.entrySet()) {
+			values[i++] = entry.getValue();
+		}
+		LocalDateTime currentHour = LocalDateTime.now(this.clock).withNano(0).withMinute(0).withSecond(0);
+
+		HourlyPrediction hourlyPrediction = new HourlyPrediction(values, currentHour);
+		return hourlyPrediction;
+	}
+
+}
---
+++ io.openems.edge.predictor.similardaymodel/src/io/openems/edge/predictor/similardaymodel/consumption/ConsumptionPredictor.java
@@ -0,0 +1,115 @@
+package io.openems.edge.predictor.similardaymodel.consumption;
+
+import java.util.Collection;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+import org.osgi.service.metatype.annotations.Designate;
+
+import io.openems.common.OpenemsConstants;
+import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
+import io.openems.edge.common.channel.Channel;
+import io.openems.edge.common.component.ComponentManager;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.event.EdgeEventConstants;
+import io.openems.edge.common.sum.Sum;
+import io.openems.edge.predictor.api.ConsumptionHourlyPredictor;
+import io.openems.edge.predictor.api.HourlyPrediction;
+import io.openems.edge.predictor.similardaymodel.AbstractSimilardayModelPredictor;
+import io.openems.edge.timedata.api.Timedata;
+
+@Designate(ocd = Config.class, factory = true)
+@Component(name = "Predictor.Consumption.SimilardayModel", //
+		immediate = true, //
+		configurationPolicy = ConfigurationPolicy.REQUIRE, //
+		property = EventConstants.EVENT_TOPIC + "=" + EdgeEventConstants.TOPIC_CYCLE_AFTER_PROCESS_IMAGE)
+
+public class ConsumptionPredictor extends AbstractSimilardayModelPredictor
+		implements ConsumptionHourlyPredictor, OpenemsComponent, EventHandler {
+
+	@Reference
+	protected ComponentManager componentManager;
+	
+	@Reference
+	protected Timedata timedata = null;
+
+	public ConsumptionPredictor() {
+		super(OpenemsConstants.SUM_ID, Sum.ChannelId.CONSUMPTION_ACTIVE_ENERGY);
+	}
+
+	@Activate
+	void activate(ComponentContext context, Config config) throws OpenemsNamedException {
+		super.activate(context, config.id(), config.alias(), config.enabled());
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+	@Override
+	protected ComponentManager getComponentManager() {
+		return this.componentManager;
+	}
+	
+	@Override
+	protected Timedata getTimedata() {
+		return this.timedata;
+	}
+
+	@Override
+	public HourlyPrediction get24hPrediction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String id() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String alias() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ComponentContext getComponentContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Channel<?> _channel(String channelName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Channel<?>> channels() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
